e2b5fe6f49377efc524803f78f2c468887dbeeeb
Marcin <serine@o2.pl>
2018-01-07 19:53:24 +0000

    switch to branch on hardcoded repo

--- hook.py
+++ hook.py
@@ -1,5 +1,7 @@
 from github_webhook import Webhook
 from flask import Flask
+from git import Repo
+import os
 
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app) # Defines '/postreceive' endpoint
@@ -10,7 +12,19 @@ def hello_world():
 
 @webhook.hook()        # Defines a handler for the 'push' event
 def on_push(data):
-    print("Got push with: {0}".format(data))
+    name = data['repository']['name']
+    branch = data['ref'].split('/')[-1]
+    commits = data['commits']
+    print("[{2}] Got push on {0} with: {1} commits:".format(branch, len(commits), name))
+
+    for ci in commits:
+        print " ", ci['message']
+
+    repo = Repo('copy')
+    git = repo.git
+    git.checkout(branch)
+    git.fetch()
+    git.pull()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
