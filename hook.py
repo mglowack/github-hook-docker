e91b24af217185f5c744a1da1fd75146010d280f
Marcin <serine@o2.pl>
2018-01-07 20:24:27 +0000

    clone repo if doesn't exist, use repo name

--- hook.py
+++ hook.py
@@ -18,13 +18,19 @@ def on_push(data):
     print("[{2}] Got push on {0} with: {1} commits:".format(branch, len(commits), name))
 
     for ci in commits:
-        print " ", ci['message']
+        print "-", ci['message']
 
-    repo = Repo('copy')
+    if not os.path.isdir(name):
+        print 'cloning repo: {0}'.format(url)
+        Repo.clone_from(url, name)
+
+    repo = Repo(name)
     git = repo.git
     git.checkout(branch)
     git.fetch()
     git.pull()
+
+    print 'switched to branch: {0}'.format(branch)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
